c28c28ba4ea9d602dc697a741a16f757a02aa940
NewtalentsG <newtalents403@gmail.com>
2025-12-29 22:55:55 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+      <ThemeSwitcher />
+    </ThemeProvider>
+  );
 }
