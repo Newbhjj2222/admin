dfcdf88c4d2c78cafef90c7498af3eff5368d267
NewtalentsG <newtalents403@gmail.com>
2025-12-31 09:07:01 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
+import Net from "@/components/Net"; // import Net component
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <Net />  {/* Iyi izajya ihora hejuru */}
+      <Component {...pageProps} /> {/* content z’izi pages zizaza munsi ya Net */}
       <ThemeSwitcher />
     </ThemeProvider>
   );
